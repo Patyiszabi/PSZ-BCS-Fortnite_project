9fe177fca22da20e96e1a4376f45b31dfb69f01e
Patyi Szabolcs <patyi.szabolcs@diak.szbi-pg.hu>
2025-11-24 09:48:35 +0100

    11.24

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_game_title = new System.Windows.Forms.Label();
+            this.ptb_avatar = new System.Windows.Forms.PictureBox();
+            this.ptb_weapon1 = new System.Windows.Forms.PictureBox();
+            this.ptb_weapon2 = new System.Windows.Forms.PictureBox();
+            this.ptb_weapon3 = new System.Windows.Forms.PictureBox();
+            this.ptb_weapon4 = new System.Windows.Forms.PictureBox();
+            this.ptb_weapon5 = new System.Windows.Forms.PictureBox();
+            this.ptb_item1 = new System.Windows.Forms.PictureBox();
+            this.ptb_item2 = new System.Windows.Forms.PictureBox();
+            this.ptb_item3 = new System.Windows.Forms.PictureBox();
+            this.ptb_item4 = new System.Windows.Forms.PictureBox();
+            this.ptb_item5 = new System.Windows.Forms.PictureBox();
+            this.ptb_item6 = new System.Windows.Forms.PictureBox();
+            this.ptb_item7 = new System.Windows.Forms.PictureBox();
+            this.ptb_item8 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_avatar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_game_title
+            // 
+            this.lbl_game_title.AutoSize = true;
+            this.lbl_game_title.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lbl_game_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_game_title.Location = new System.Drawing.Point(955, 9);
+            this.lbl_game_title.Name = "lbl_game_title";
+            this.lbl_game_title.Size = new System.Drawing.Size(132, 39);
+            this.lbl_game_title.TabIndex = 0;
+            this.lbl_game_title.Text = "Fortnite";
+            // 
+            // ptb_avatar
+            // 
+            this.ptb_avatar.Location = new System.Drawing.Point(42, 57);
+            this.ptb_avatar.Name = "ptb_avatar";
+            this.ptb_avatar.Size = new System.Drawing.Size(260, 430);
+            this.ptb_avatar.TabIndex = 1;
+            this.ptb_avatar.TabStop = false;
+            // 
+            // ptb_weapon1
+            // 
+            this.ptb_weapon1.Location = new System.Drawing.Point(42, 509);
+            this.ptb_weapon1.Name = "ptb_weapon1";
+            this.ptb_weapon1.Size = new System.Drawing.Size(160, 160);
+            this.ptb_weapon1.TabIndex = 2;
+            this.ptb_weapon1.TabStop = false;
+            // 
+            // ptb_weapon2
+            // 
+            this.ptb_weapon2.Location = new System.Drawing.Point(208, 509);
+            this.ptb_weapon2.Name = "ptb_weapon2";
+            this.ptb_weapon2.Size = new System.Drawing.Size(160, 160);
+            this.ptb_weapon2.TabIndex = 2;
+            this.ptb_weapon2.TabStop = false;
+            // 
+            // ptb_weapon3
+            // 
+            this.ptb_weapon3.Location = new System.Drawing.Point(374, 509);
+            this.ptb_weapon3.Name = "ptb_weapon3";
+            this.ptb_weapon3.Size = new System.Drawing.Size(160, 160);
+            this.ptb_weapon3.TabIndex = 2;
+            this.ptb_weapon3.TabStop = false;
+            // 
+            // ptb_weapon4
+            // 
+            this.ptb_weapon4.Location = new System.Drawing.Point(540, 509);
+            this.ptb_weapon4.Name = "ptb_weapon4";
+            this.ptb_weapon4.Size = new System.Drawing.Size(160, 160);
+            this.ptb_weapon4.TabIndex = 2;
+            this.ptb_weapon4.TabStop = false;
+            // 
+            // ptb_weapon5
+            // 
+            this.ptb_weapon5.Location = new System.Drawing.Point(706, 509);
+            this.ptb_weapon5.Name = "ptb_weapon5";
+            this.ptb_weapon5.Size = new System.Drawing.Size(160, 160);
+            this.ptb_weapon5.TabIndex = 2;
+            this.ptb_weapon5.TabStop = false;
+            // 
+            // ptb_item1
+            // 
+            this.ptb_item1.Location = new System.Drawing.Point(362, 57);
+            this.ptb_item1.Name = "ptb_item1";
+            this.ptb_item1.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item1.TabIndex = 2;
+            this.ptb_item1.TabStop = false;
+            // 
+            // ptb_item2
+            // 
+            this.ptb_item2.Location = new System.Drawing.Point(468, 57);
+            this.ptb_item2.Name = "ptb_item2";
+            this.ptb_item2.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item2.TabIndex = 2;
+            this.ptb_item2.TabStop = false;
+            // 
+            // ptb_item3
+            // 
+            this.ptb_item3.Location = new System.Drawing.Point(574, 57);
+            this.ptb_item3.Name = "ptb_item3";
+            this.ptb_item3.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item3.TabIndex = 2;
+            this.ptb_item3.TabStop = false;
+            // 
+            // ptb_item4
+            // 
+            this.ptb_item4.Location = new System.Drawing.Point(362, 163);
+            this.ptb_item4.Name = "ptb_item4";
+            this.ptb_item4.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item4.TabIndex = 2;
+            this.ptb_item4.TabStop = false;
+            // 
+            // ptb_item5
+            // 
+            this.ptb_item5.Location = new System.Drawing.Point(468, 163);
+            this.ptb_item5.Name = "ptb_item5";
+            this.ptb_item5.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item5.TabIndex = 2;
+            this.ptb_item5.TabStop = false;
+            // 
+            // ptb_item6
+            // 
+            this.ptb_item6.Location = new System.Drawing.Point(574, 163);
+            this.ptb_item6.Name = "ptb_item6";
+            this.ptb_item6.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item6.TabIndex = 2;
+            this.ptb_item6.TabStop = false;
+            // 
+            // ptb_item7
+            // 
+            this.ptb_item7.Location = new System.Drawing.Point(414, 269);
+            this.ptb_item7.Name = "ptb_item7";
+            this.ptb_item7.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item7.TabIndex = 2;
+            this.ptb_item7.TabStop = false;
+            // 
+            // ptb_item8
+            // 
+            this.ptb_item8.Location = new System.Drawing.Point(520, 269);
+            this.ptb_item8.Name = "ptb_item8";
+            this.ptb_item8.Size = new System.Drawing.Size(100, 100);
+            this.ptb_item8.TabIndex = 2;
+            this.ptb_item8.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(1069, 591);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kilépés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ptb_weapon5);
+            this.Controls.Add(this.ptb_weapon4);
+            this.Controls.Add(this.ptb_weapon3);
+            this.Controls.Add(this.ptb_weapon2);
+            this.Controls.Add(this.ptb_item8);
+            this.Controls.Add(this.ptb_item6);
+            this.Controls.Add(this.ptb_item3);
+            this.Controls.Add(this.ptb_item5);
+            this.Controls.Add(this.ptb_item2);
+            this.Controls.Add(this.ptb_item7);
+            this.Controls.Add(this.ptb_item4);
+            this.Controls.Add(this.ptb_item1);
+            this.Controls.Add(this.ptb_weapon1);
+            this.Controls.Add(this.ptb_avatar);
+            this.Controls.Add(this.lbl_game_title);
+            this.Name = "Form1";
+            this.Text = "Fortnite";
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_avatar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_weapon5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_item8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_game_title;
+        private System.Windows.Forms.PictureBox ptb_avatar;
+        private System.Windows.Forms.PictureBox ptb_weapon1;
+        private System.Windows.Forms.PictureBox ptb_weapon2;
+        private System.Windows.Forms.PictureBox ptb_weapon3;
+        private System.Windows.Forms.PictureBox ptb_weapon4;
+        private System.Windows.Forms.PictureBox ptb_weapon5;
+        private System.Windows.Forms.PictureBox ptb_item1;
+        private System.Windows.Forms.PictureBox ptb_item2;
+        private System.Windows.Forms.PictureBox ptb_item3;
+        private System.Windows.Forms.PictureBox ptb_item4;
+        private System.Windows.Forms.PictureBox ptb_item5;
+        private System.Windows.Forms.PictureBox ptb_item6;
+        private System.Windows.Forms.PictureBox ptb_item7;
+        private System.Windows.Forms.PictureBox ptb_item8;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,63 @@ namespace PSZ_BCS_Fortnite_project
         {
             InitializeComponent();
         }
+        public static string weapon_file = "fegyverek.txt";
+        public static string item_file = "itemek.txt";
+        public static string player_file = "jatekosok.txt";
+
+        public class jatekos
+        {
+            public string username;
+            public int level;
+            public int life;
+            public int shield;
+            public int kills;
+            public int xp;
+            public int weapons;
+            public int items;
+            public int avatar;
+
+            public jatekos(string username, int level, int life, int shield, int kills, int xp, int weapons, int items, int avatar)
+            {
+                this.username = username;
+                this.level = level;
+                this.life = life;
+                this.shield = shield;
+                this.kills = kills;
+                this.xp = xp;
+                this.weapons = weapons;
+                this.items = items;
+                this.avatar = avatar;
+            }
+        }
+        public class weapons_class
+        {
+            public string weapon_name;
+            public int level;
+            public int mag_size;
+            public weapons_class(string weapon_name, int level, int mag_size)
+            {
+                this.weapon_name = weapon_name;
+                this.level = level;
+                this.mag_size = mag_size;
+            }
+        }
+        public class items_class
+        {
+            public string item_name;
+            public int quantity;
+            public items_class(string item_name, int quantity)
+            {
+                this.item_name = item_name;
+                this.quantity = quantity;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Köszönjük, hogy játékunkat használta! A ranglista elérhető");
+            Application.Exit();
+           
+        }
     }
 }
